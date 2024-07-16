bcfadeccf38e4ab416e1831d030d7dcec65e668d
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2024-07-16 11:03:11 +0200

    gauging value of first state

--- tests/test_example_states.py
+++ tests/test_example_states.py
@@ -15,22 +15,59 @@ def test_eval_all_states():
         x.append(state_to_features(state.get_state_info()))
 
     x = torch.tensor(np.array(x), dtype=torch.float32)
-    print(x.dtype)
 
     agent = NNValueAgent(game_instance=RoyalGameOfUr(),
                          models_dir_path='../ur_models',
-                         depth=2,
                          )
 
     y = agent.value_function(x).detach().numpy()
     print(np.round(y, 3))
 
 
+def test_eval_initial_states():
+
+    x = []
+    info_0 = STATES[-1]
+
+    for i in range(5):
+        info = info_0
+        info['n_steps'] = i
+        state = RoyalGameOfUr().set_state(info)
+        x.append(state_to_features(state.get_state_info()))
+
+    x = torch.tensor(np.array(x), dtype=torch.float32)
+
+    agent = NNValueAgent(game_instance=RoyalGameOfUr(),
+                         models_dir_path='../ur_models',
+                         )
+
+    y = agent.value_function(x).detach().numpy()[:,0]
+    w_0 = np.array([1, 4, 6, 4, 1]) / 16
+    w_1 = np.array([0, 4, 6, 4, 1]) / 15
+
+    value_0 = y[0]
+    value_1 = y.dot(w_1)
+    value_2 = (16-15*value_1)/17
+    value_avg = y.dot(w_0)
+
+    print()
+    for i in range(len(y)):
+        print(f'{i})  {y[i]:.2%}')
+
+    print()
+    print(f'avg_val = {value_avg:.2%}')
+
+    print()
+    print(f'value_0 = {value_0:5.2%}')
+    # print(f'value_1 = {value_1:.1%}')
+    print(f'          {value_2:5.2%}')
+    print(f'          {value_0 - value_2:+5.2%}   <-  (should be 0%)')
+
+
 def test_get_action():
     state = RoyalGameOfUr().set_state(STATES[0])
     agent = NNValueAgent(game_instance=RoyalGameOfUr(),
                          models_dir_path='../ur_models',
-                         depth=2,
                          )
 
     print(state)
@@ -39,4 +76,5 @@ def test_get_action():
 
 if __name__ == '__main__':
     # test_eval_all_states()
-    test_get_action()
+    test_eval_initial_states()
+    # test_get_action()
