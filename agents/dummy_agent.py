231668f438308ef73989bb32539ca649f62e2e5b
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2024-07-18 15:30:21 +0200

    +DummyAgent2

--- agents/dummy_agent.py
+++ agents/dummy_agent.py
@@ -3,8 +3,7 @@ from game.agent import Agent
 
 
 class DummyAgent(Agent):
-    """Handcrafted bot (elo=370)"""
-
+    """Handcrafted bot (elo = 370)"""
     def __init__(self, policy=None, greedy=False, policy_path=None):
         self.policy = policy
         self.greedy = greedy
@@ -18,6 +17,24 @@ class DummyAgent(Agent):
         """move the last piece in the list of legal moves"""
         moves = state_info["legal_move_indices"]
         return {"action": moves[-1], "eval": np.ones(2) * 0.5}
+
+
+class DummyAgent2(DummyAgent):
+    """Handcrafted bot (elo = 480 = DummyAgent + 110)"""
+    def get_action(self, state_info: dict, verbose=False) -> dict:
+        """move the last piece in the list of legal moves,
+        but tries to take opponent's pieces"""
+        moves = state_info["legal_move_indices"]
+        roll = state_info["n_steps"]
+        opp_board = state_info["board"][1 - state_info["current_player"]]
+        move = moves[-1]
+        if move <= 3:
+            move = moves[0]
+        if len(moves) > 1:
+            x = moves[-2] + roll
+            if opp_board[x]:
+                move = moves[-2]
+        return {"action": move, "eval": np.ones(2) * 0.5}
 
 
 class GreedyAgent(DummyAgent):
